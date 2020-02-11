b1a8ea2c9fd0ddfdb614c44ae2e664c3db3fc780
nguyenven299 <nguyenven1999@gmail.com>
2020-02-11 10:28:19 +0700

    lap trinh android

---
+++ Chat2020_V3/app/src/main/java/com/nguyenven/chat2020_v3/MainActivity.java
@@ -0,0 +1,129 @@
+package com.nguyenven.chat2020_v3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGhiAm, btnXong, btnGui;
+
+    private MediaRecorder myRecorder;
+    private MediaPlayer myPlayer;
+    private String outputFile = null;
+    private Socket mSocket;
+
+
+    {
+        try {
+            mSocket = IO.socket("http://192.168.0.107:4100");
+        } catch (URISyntaxException e) {
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSocket.connect();
+
+        outputFile = Environment.getExternalStorageState() + "/nguyenven.3gpp";
+        myRecorder = new MediaRecorder();
+        myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myRecorder.setOutputFile(outputFile);
+
+        btnGhiAm = (Button) findViewById(R.id.buttonGhiAm);
+        btnGhiAm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                start(v);
+            }
+        });
+
+        btnXong = (Button) findViewById(R.id.buttonXong);
+        btnXong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                stop(v);
+            }
+        });
+
+        btnGui = (Button) findViewById(R.id.buttonGui);
+        btnGui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String path  =outputFile = Environment.getExternalStorageDirectory().
+                        getAbsolutePath() + "/nguyenven.3gpp";
+                byte[] amthanh = FileLocal_To_Byte(path);
+                mSocket.emit("client-gui-am-thanh",amthanh );
+
+            }
+        });
+    }
+
+    public void start(View view){
+        try {
+            myRecorder.prepare();
+            myRecorder.start();
+            Toast.makeText(getApplicationContext(), "Start recording...",
+                    Toast.LENGTH_SHORT).show();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop(View view){
+        try {
+            myRecorder.stop();
+            myRecorder.release();
+            myRecorder  = null;
+
+            Toast.makeText(getApplicationContext(), "Stop recording...",
+                    Toast.LENGTH_SHORT).show();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+    public byte[] FileLocal_To_Byte(String path){
+        File file = new File(path);
+        int size = (int) file.length();
+        byte[] bytes = new byte[size];
+        try {
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+            buf.read(bytes, 0, bytes.length);
+            buf.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return bytes;
+    }
+}
